a3b47e92c6b70e0e0570965d3a812f6a0022d075
Rohan Shrothrium <rohan.shrothrium@intellecteu.com>
2022-07-17 22:37:02 +0530

    craYZZ praV d MaN
    Signed-off-by: Rohan Shrothrium <rohan.shrothrium@intellecteu.com>

--- controllers/hspaController.js
+++ controllers/hspaController.js
@@ -7,49 +7,40 @@ const axios = require('axios');
 const dbPath = '../db/dbConfig.json'
 const jsonPath = path.join(__dirname, '..', 'db', 'dbConfig.json');
 
-// exports.RegisterAmbulance = async (vehicalNumber, location) => {
-//     try {
-//         let rawdata = fs.readFileSync(jsonPath);
-//         let catalogue = JSON.parse(rawdata);
+exports.RegisterAmbulance = async (vehicleNumber) => {
+    try {
+        let rawdata = fs.readFileSync(jsonPath);
+        let catalogue = JSON.parse(rawdata);
+        var uuid = randomUUID()
+        var newUser = {
+            id: uuid,
+            type: "Ambulance",
+            agent: {},
+            start: {
+                time: {
+                    timestamp: ""
+                }
+            },
+            end: {
+                time: {
+                    timestamp: ""
+                }
+            }
+        }
+        catalogue.fulfillments.push(newUser)
+        catalogue.items.push({ ...constants.ITEM, id: uuid, fulfillment_id: uuid, descriptor: { name: "Ambulance - " + vehicleNumber } })
 
+        fs.writeFile(jsonPath, JSON.stringify(catalogue), function (err) {
+            if (err) throw err;
+            console.log("It's saved!");
+            // file written successfully
+        });
 
-//         var startTimeAsDate = new Date(startTime)
-//         var endTimeAsDate = new Date(endTime)
-
-//         while (startTimeAsDate <= endTimeAsDate) {
-//             var uuid = randomUUID()
-//             var newUser = {
-//                 id: uuid,
-//                 type: "Lab Booking " + type,
-//                 agent: {},
-//                 start: {
-//                     time: {
-//                         timestamp: startTimeAsDate.toISOString()
-//                     }
-//                 },
-//                 end: {
-//                     time: {
-//                         timestamp: addMinutes(startTimeAsDate, 30).toISOString()
-//                     }
-//                 }
-//             }
-
-//             startTimeAsDate = addMinutes(startTimeAsDate, 30)
-//             catalogue.fulfillments.push(newUser)
-//             catalogue.items.push({ ...constants.ITEM, id: uuid, fulfillment_id: uuid, descriptor: { name: type } })
-//         }
-
-//         fs.writeFile(jsonPath, JSON.stringify(catalogue), function (err) {
-//             if (err) throw err;
-//             console.log("It's saved!");
-//             // file written successfully
-//         });
-
-//         return { status: "success" }
-//     } catch (err) {
-//         return { err }
-//     }
-// }
+        return { status: "success" }
+    } catch (err) {
+        return { err }
+    }
+}
 
 exports.RegisterLabTest = async (startTime, endTime, type) => {
     try {
@@ -200,6 +191,31 @@ exports.GetLabBookings = async () => {
     }
 }
 
+exports.GetAmbulances = async () => {
+    try {
+        let rawdata = fs.readFileSync(jsonPath);
+        let catalogue = JSON.parse(rawdata);
+
+        var users = []
+        for (let i = 0; i < catalogue.fulfillments.length; i++) {
+            if (!catalogue.fulfillments[i].type.includes("Ambulance")) {
+                continue;
+            }
+            var user = {
+                type: catalogue.fulfillments[i].type,
+                vehicleNumber: catalogue.items[i].descriptor.name,
+                isBooked: catalogue.fulfillments[i].isBooked != undefined
+            }
+            console.log(user)
+            users.push(user)
+        }
+
+        return { status: "success", users }
+    } catch (err) {
+        return { err }
+    }
+}
+
 exports.Search = async (context, intent) => {
     try {
         let rawdata = fs.readFileSync(jsonPath);
@@ -209,11 +225,11 @@ exports.Search = async (context, intent) => {
             context: context,
             message: {
                 catalog: {
-                    descriptor: {name: "thecodingcompany"},
+                    descriptor: { name: "thecodingcompany" },
                     fulfillments: [],
-                    items: [] 
-                } 
-            } 
+                    items: []
+                }
+            }
         }
         var reqStartTime = new Date(intent.fulfillment.start.time.timestamp)
         var reqEndTime = new Date(intent.fulfillment.end.time.timestamp)
@@ -228,13 +244,62 @@ exports.Search = async (context, intent) => {
             // todo: more validations??
         }
         response.context.provider_id = "thecodingcompany.herokuapp.com"
-        response.context.provider_uri = "https://f0ee-122-162-231-10.in.ngrok.io/hspa"
+        response.context.provider_uri = "https://f0ee-122-162-231-10.in.ngrok.io/hspa/service"
         response.context.action = "on_search"
         response.context.message_id = context.message_id
 
         // send post request to gateway
         axios
             .post('http://121.242.73.120:8083/api/v1/on_search', response)
+            .then(res => {
+                console.log(`statusCode: ${res.status}`);
+                console.log(res.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        return { response: "success" }
+    } catch (err) {
+        return { err }
+    }
+}
+
+exports.ServiceSearch = async (context, intent) => {
+    try {
+        let rawdata = fs.readFileSync(jsonPath);
+        let catalogue = JSON.parse(rawdata);
+
+        var response = {
+            context: context,
+            message: {
+                catalog: {
+                    descriptor: { name: "thecodingcompany" },
+                    fulfillments: [],
+                    items: []
+                }
+            }
+        }
+        var reqStartTime = new Date(intent.fulfillment.start.time.timestamp)
+        var reqEndTime = new Date(intent.fulfillment.end.time.timestamp)
+        for (let i = 0; i < catalogue.fulfillments.length; i++) {
+            if (validateFulfilmentTime(catalogue.fulfillments[i], reqStartTime, reqEndTime) &&
+                validateFulfillmentTags(catalogue.fulfillments[i], intent.fulfillment.agent) &&
+                validateFulfillmentType(catalogue.fulfillments[i], intent.fulfillment.type) &&
+                catalogue.fulfillments[i].isBooked == undefined) {
+                response.message.catalog.fulfillments.push(catalogue.fulfillments[i])
+                response.message.catalog.items.push(catalogue.items[i])
+            }
+            // todo: more validations??
+        }
+        response.context.provider_id = "thecodingcompany.herokuapp.com"
+        response.context.provider_uri = "https://f0ee-122-162-231-10.in.ngrok.io/hspa/service"
+        response.context.action = "on_search"
+        response.context.message_id = context.message_id
+
+        // send post request to gateway
+        axios
+            .post(context.consumer_uri + '/on_search', response)
             .then(res => {
                 console.log(`statusCode: ${res.status}`);
                 console.log(res.data);
@@ -327,11 +392,18 @@ exports.Confirm = async (context, order) => {
 }
 
 function validateFulfillmentType(fulfillment, type) {
-    var s = similarity(fulfillment.type, type)
-    if (fulfillment.agent.name == undefined){
-        return s > 0.4
+    try {
+        var s = similarity(fulfillment.type, type)
+        if (fulfillment.type.includes('Lab Booking')) {
+            updatedFulfillmentType = fulfillment.type.replace('Lab Booking ', '')
+            updatedType = type.replace('Lab Booking ', '')
+            s = similarity(updatedFulfillmentType, updatedType)
+            return s > 0.4 || updatedFulfillmentType.includes(updatedType)
+        }
+        return s > 0.6
+    } catch (err) {
+        return false
     }
-    return s > 0.6
 }
 
 function validateFulfilmentTime(fulfilment, reqStartTime, reqEndTime) {
@@ -361,29 +433,33 @@ function validateFulfilmentExists(fulfillment, item) {
 }
 
 function validateFulfillmentTags(fulfillment, agent) {
-    if (fulfillment.agent.type === {}) {
+    try {
+        if (fulfillment.agent.type === {}) {
+            return true
+        }
+
+        for (key in agent.tags) {
+            if (!JSON.stringify(fulfillment.agent).includes(agent.tags[key])) {
+                return false
+            }
+        }
+
+        if (agent.name != "" && agent.name != undefined && agent.name != null) {
+            if (!fulfillment.agent.name.includes(agent.name)) {
+                return false
+            }
+        }
+
+        var x = agent.id
+        if (agent.id != "" && agent.id != undefined && agent.id != null) {
+            if (fulfillment.agent.id != agent.id) {
+                return false
+            }
+        }
         return true
+    } catch (err) {
+        return false
     }
-
-    for (key in agent.tags) {
-        if (!JSON.stringify(fulfillment.agent).includes(agent.tags[key])) {
-            return false
-        }
-    }
-
-    if (agent.name != "" && agent.name != undefined && agent.name != null) {
-        if (!fulfillment.agent.name.includes(agent.name)) {
-            return false
-        }
-    }
-
-    var x = agent.id
-    if (agent.id != "" && agent.id != undefined && agent.id != null) {
-        if (fulfillment.agent.id != agent.id) {
-            return false
-        }
-    }
-    return true
 }
 
 function addMinutes(date, minutes) {
@@ -431,13 +507,11 @@ function editDistance(s1, s2) {
     return costs[s2.length];
 }
 
-function sort_by_key(array, key)
-{
- return array.sort(function(a, b)
- {
-  var x = a[key]; var y = b[key];
-  return ((x < y) ? -1 : ((x > y) ? 1 : 0));
- });
+function sort_by_key(array, key) {
+    return array.sort(function (a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
 
 const PAYMENT = {
--- routes/hspaRoutes.js
+++ routes/hspaRoutes.js
@@ -5,19 +5,25 @@ const app = express();
 
 app.post('/search', async (req, resp) => {
     const { message, context } = req.body
-    hspaService.Search(context, message.intent)
+    hspaService.Search(context, message?.intent)
         .then(result => resp.status(200).json(result))
 });
 
-app.post('/init', async (req, resp) => {
+app.post('/service/search', async (req, resp) => {
     const { message, context } = req.body
-    hspaService.Init(context, message.order)
+    hspaService.Search(context, message?.intent)
+        .then(result => resp.status(200).json(result))
+});
+
+app.post('/service/init', async (req, resp) => {
+    const { message, context } = req.body
+    hspaService.Init(context, message?.order)
         .then(result => resp.status(200).json(result))
 })
 
-app.post('/confirm', async (req, resp) => {
+app.post('/service/confirm', async (req, resp) => {
     const { message, context } = req.body
-    hspaService.Confirm(context, message.order)
+    hspaService.Confirm(context, message?.order)
         .then(result => resp.status(200).json(result))
 })
 
@@ -33,6 +39,12 @@ app.post('/registerLabTest', async (req, resp) => {
         .then(result => resp.status(200).json(result))
 })
 
+app.post('/registerAmbulance', async (req, resp) => {
+    const { vehicleNumber } = req.body
+    hspaService.RegisterAmbulance(vehicleNumber)
+        .then(result => resp.status(200).json(result))
+})
+
 app.post('/getDoctors', async (req, resp) => {
     hspaService.GetAppointments()
         .then(result => resp.status(200).json(result))
@@ -40,6 +52,11 @@ app.post('/getDoctors', async (req, resp) => {
 
 app.post('/getLabTests', async (req, resp) => {
     hspaService.GetLabBookings()
+        .then(result => resp.status(200).json(result))
+})
+
+app.post('/getAmbulances', async (req, resp) => {
+    hspaService.GetAmbulances()
         .then(result => resp.status(200).json(result))
 })
 
